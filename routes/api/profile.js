db70eaf16b8318da3c0f1d92338b5f6c2d60af6e
oeawFebe <oeawFebe@gmail.com>
2021-06-21 15:44:42 -0400

    auto

---
+++ routes/api/profile.js
@@ -0,0 +1,11 @@
+const { response } = require('express');
+const express=require('express');
+
+const router=express.Router();
+
+// @route  GET api/profile
+// @desc   Test route
+// @access Public
+router.get('/',(req,res)=>res.send('Profile route'));
+
+module.exports=router;
--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express=require('express');
+const connectDB=require('./config/db');
+
 const app=express();
+
+//Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}));
+
 app.get('/',(req,res)=>{
     res.send('API Running');
 });
+
+//define Routes
+app.use('/api/user',require('./routes/api/user'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
 const PORT=process.env.PORT||5000;
 app.listen(PORT,()=>console.log(`server started on port ${PORT}`));
